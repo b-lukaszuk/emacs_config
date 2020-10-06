94422c71c95d6a05a60060a5186a84cbbbb619d3
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2020-10-06 18:36:39 +0200

    emacs - moj config (06-10-2020)

---
+++ init.el
@@ -0,0 +1,42 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+(require 'package)
+(package-initialize)
+(require 'cl)
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+
+;; If there are no archived package contents, refresh them
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(eval-when-compile
+  (require 'use-package))
+
+(require 'org)
+(org-babel-load-file
+  (expand-file-name "./myinit.org"
+		    user-emacs-directory))
+(setq inhibit-splash-screen t)
+(set-default-font "Monospace 12")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ein:output-area-inlined-images t)
+ '(package-selected-packages (quote (highlight-indent-guides seq helm evil use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'erase-buffer 'disabled nil)
