dbbd0df57658e12e3c206124931fb664ac62107b
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2020-10-09 10:27:39 +0200

    Drobne zmiany + dodanie linii pokazujacej poziom wciecia.

--- init.el
+++ init.el
@@ -30,7 +30,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ein:output-area-inlined-images t)
  '(package-selected-packages (quote (highlight-indent-guides seq helm evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
