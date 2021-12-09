eb76d076c56bf19ad164c9627de84e626eceeb7b
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-12-09 12:20:43 +0100

    poprawka indentacji w haskellowym modzie

--- init.el
+++ init.el
@@ -38,6 +38,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ecb-options-version "2.50")
+ '(evil-auto-indent t)
  '(package-selected-packages
    (quote
     (lsp-java exwm highlight-indent-guides seq helm evil use-package))))
