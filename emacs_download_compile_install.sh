7c7c331b81c55b91e9d55434d91ab7f43a30c0c9
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-09-13 19:47:30 +0200

    small update for emacs 29

--- emacs_download_compile_install.sh
+++ emacs_download_compile_install.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# be careful, make sure to know what you are doing, use it at your own risk
+
 # prerequisites
-sudo apt install build-essential libgif-dev libgnutls28-dev libgtk2.0-dev libjpeg-dev libncurses-dev libpng-dev libtiff-dev libx11-dev libxpm-dev texinfo
+sudo apt install build-essential libgif-dev libgnutls28-dev libgtk2.0-dev libjpeg-dev libncurses-dev libpng-dev libtiff-dev libx11-dev libxpm-dev texinfo libjansson-dev
 
 # downloading emacs from the official gnu repository (ftp server)
-wget https://ftp.gnu.org/gnu/emacs/emacs-27.2.tar.gz # change emacs version here
-tar xvzf emacs-27.2.tar.gz # change emacs version here
+wget https://ftp.gnu.org/gnu/emacs/emacs-29.1.tar.gz # change emacs version here
 
-cd emacs-27.2 || exit # change emacs version here
-./configure --with-native-compilation --with-json # or just ./configure
+# to download *.sig file
+# wget https://ftp.gnu.org/gnu/emacs/emacs-29.1.tar.gz.sig # change emacs version here
+# info about *sig files: https://ubuntuforums.org/showthread.php?t=1946900
+
+# to get gnu-keyring-gpg
+# wget https://ftp.gnu.org/gnu/gnu-keyring.gpg
+# info about gnu-keyring.gpg: https://stackoverflow.com/questions/5701388/where-can-i-find-the-public-key-for-gnu-emacs
+
+tar xvzf emacs-29.1.tar.gz # change emacs version here
+
+cd emacs-29.1 || exit # change emacs version here
+./configure --with-json # or just ./configure
 make
 sudo make install
